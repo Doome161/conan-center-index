527d4046ddf0e632cd96127895512664ab6dd3b2
doome161 <schulze.dominik@gmx.de>
2022-11-06 19:06:29 +0100

    ctml: conan v2 migration

--- recipes/ctml/all/conanfile.py
+++ recipes/ctml/all/conanfile.py
@@ -1,7 +1,12 @@
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.files import get, copy
+from conan.tools.layout import basic_layout
+
 import os
-from conans import ConanFile, tools
 
 required_conan_version = ">=1.43.0"
+
 
 class CtmlLibrariesConan(ConanFile):
     name = "ctml"
@@ -11,31 +16,33 @@ class CtmlLibrariesConan(ConanFile):
     homepage = "https://github.com/tinfoilboy/CTML"
     topics = ("generator", "html", )
     settings = "os", "arch", "compiler", "build_type",
-    generators = "cmake",
     no_copy_source = True
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def layout(self):
+        basic_layout(self)
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "11")
+            check_min_cppstd(self, "11")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
 
     def package(self):
-        self.copy("LICENSE*", "licenses", self._source_subfolder)
-        self.copy("ctml.hpp", "include", os.path.join(self._source_subfolder, "include"))
+        copy(self, "LICENSE*", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(self, "ctml.hpp", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "include"))
 
     def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
         self.cpp_info.set_property("cmake_file_name", "CTML")
         self.cpp_info.set_property("cmake_target_name", "CTML::CTML")
 
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
         self.cpp_info.filenames["cmake_find_package"] = "CTML"
         self.cpp_info.filenames["cmake_find_package_multi"] = "CTML"
         self.cpp_info.names["cmake_find_package"] = "CTML"
